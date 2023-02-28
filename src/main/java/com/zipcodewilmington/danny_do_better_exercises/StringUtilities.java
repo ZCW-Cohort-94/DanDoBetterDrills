dfb9cfb6fbe2ac6d8b6a4f0577d8ed6876555fb2
Adam B <adamb@Zipcoders-MacBook-Pro.local>
2023-02-28 15:45:27 -0500

    Completed tests cases

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
@@ -8,7 +8,7 @@ public class StringUtilities {
      * @return `Hello World` as a string
      */
     public static String getHelloWorld() {
-        return null;
+        return "Hello World";
     }
 
     /**
@@ -17,7 +17,8 @@ public class StringUtilities {
      * @return the concatenation of two strings, `firstSegment`, and `secondSegment`
      */
     public static String concatenation(String firstSegment, String secondSegment){
-        return null;
+
+        return (firstSegment + secondSegment);
     }
 
     /**
@@ -26,7 +27,8 @@ public class StringUtilities {
      * @return the concatenation of an integer, `firstSegment`, and a String, `secondSegment`
      */
     public static String concatenation(int firstSegment, String secondSegment){
-        return null;
+
+        return (firstSegment + secondSegment);
     }
 
     /**
@@ -34,7 +36,8 @@ public class StringUtilities {
      * @return the first 3 characters of `input`
      */
     public static String getPrefix(String input){
-        return null;
+        
+        return (input.substring(0,3));
     }
 
     /**
@@ -42,16 +45,16 @@ public class StringUtilities {
      * @return the last 3 characters of `input`
      */
     public static String getSuffix(String input){
-        return null;
+        return input.substring(input.length()-3);
     }
 
     /**
-     * @param inputValue the value to be compared
+     * @param inputValue      the value to be compared
      * @param comparableValue the value to be compared against
      * @return the equivalence of two strings, `inputValue` and `comparableValue`
      */
-    public static Boolean compareTwoStrings(String inputValue, String comparableValue){
-        return null;
+    public static boolean compareTwoStrings(String inputValue, String comparableValue){
+        return inputValue.equals(comparableValue);
     }
 
     /**
@@ -59,7 +62,7 @@ public class StringUtilities {
      * @return the middle character of `inputValue`
      */
     public static Character getMiddleCharacter(String inputValue){
-        return null;
+        return inputValue.charAt((inputValue.length()-1)/2); //length starts at 0 remember
     }
 
     /**
@@ -67,7 +70,8 @@ public class StringUtilities {
      * @return the first sequence of characters
      */
     public static String getFirstWord(String spaceDelimitedString){
-        return null;
+        String list[] = spaceDelimitedString.split(" ");
+                return list[0];
     }
 
     /**
@@ -75,7 +79,8 @@ public class StringUtilities {
      * @return the second word of a string delimited by spaces.
      */
     public static String getSecondWord(String spaceDelimitedString){
-        return null;
+        String list[] = spaceDelimitedString.split(" ");
+        return list[1];
     }
 
     /**
@@ -83,6 +88,10 @@ public class StringUtilities {
      * @return an identical string with characters in reverse order.
      */
     public static String reverse(String stringToReverse){
-        return null;
+        String reverse = "";
+        for (int i = (stringToReverse.length()-1); i > -1; i--){
+            reverse = reverse + stringToReverse.charAt(i);
+        }
+        return reverse;
     }
 }
